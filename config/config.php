4be992fd90d3f243bcf6bc1d5b762b9aed27a4f5
JiNexus <jinvirle@gmail.com>
2024-03-11 19:30:44 +0800

    Require config/dotenv.php in configuration

--- config/config.php
+++ config/config.php
@@ -6,6 +6,8 @@ use Laminas\ConfigAggregator\ArrayProvider;
 use Laminas\ConfigAggregator\ConfigAggregator;
 use Laminas\ConfigAggregator\PhpFileProvider;
 
+require 'config/dotenv.php';
+
 // To enable or disable caching, set the `ConfigAggregator::ENABLE_CACHE` boolean in
 // `config/autoload/local.php`.
 $cacheConfig = [
