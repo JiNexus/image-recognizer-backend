b0857374bec819007c406ed2ee990028b1517859
JiNexus <jinvirle@gmail.com>
2024-03-16 02:10:41 +0800

    Add Image Collection using Laminas paginator

---
+++ src/App/src/Collection/ImageCollection.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Collection;
+
+//use Doctrine\ORM\Tools\Pagination\Paginator;
+//use Doctrine\Common\Collections\ArrayCollection;
+use Laminas\Paginator\Paginator;
+
+class ImageCollection extends Paginator { }
